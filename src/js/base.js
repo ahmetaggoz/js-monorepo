42a6daf9403ed389ec5ed638b12d5dec987e49f0
ahmetaggoz <ahmetaggoz1388@gmail.com>
2025-07-17 20:48:59 +0300

    Squashed 'IlearnedMongoDB/' content from commit 942f90d9
    git-subtree-dir: IlearnedMongoDB
    git-subtree-split: 942f90d9e08dc545619d24b01ae1f355362f945e

---
+++ src/js/base.js
@@ -0,0 +1,10 @@
+export const elements = {
+    searchForm: document.getElementById("form-search"),
+    searchInput: document.getElementById("txt-keyword"),
+    searchResult: document.getElementById("results"),
+    movieListContainer: document.getElementById("movie-list-container"),
+    movieListHeader: document.getElementById("movie-list-header"),
+    movieDetails: document.querySelector(".movie-details"),
+    movieDetailsContainer: document.getElementById("movie-details-container"),
+    movieDetailsClose: document.getElementById("movie-details-close")
+}
